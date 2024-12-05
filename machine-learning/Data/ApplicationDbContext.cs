e84f6db9bc8424d509f63ebb4e7816182e56c109
KodaJonathan <jonathanlungu9@gmail.com>
2024-12-05 12:35:23 +0200

    intial project set up

---
+++ machine-learning/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace machine_learning.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ machine-learning/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace machine_learning.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
